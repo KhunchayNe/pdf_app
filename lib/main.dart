f45c0f7ce5d549ef27799137fa23df13c7a77eb0
siwapan <work.siwapun.kh@gmail.com>
2024-12-03 15:53:40 +0700

    Add open_file and path_provider dependencies; update project configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'mobile.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+
+import 'dart:typed_data';
+import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
   runApp(const MyApp());
@@ -7,27 +13,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +29,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +36,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () => _createPdf(),
+          child: Text("Create"),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<void> _createPdf() async {
+    if (kDebugMode) print('create');
+
+    PdfDocument document = PdfDocument();
+    final page = document.pages.add();
+    
+
+    // page.graphics.drawString(
+    //       'Hello world!',
+    //       PdfStandardFont(PdfFontFamily.helvetica, 12,
+    //           style: PdfFontStyle.bold),
+    //       bounds: Rect.fromLTWH(0, 0, 200, 50),
+    //       brush: PdfBrushes.red,
+    //       pen: PdfPens.blue,
+    //       format: PdfStringFormat(alignment: PdfTextAlignment.left));
+
+    page.graphics.drawString("hello world", PdfStandardFont(PdfFontFamily.helvetica, 24));
+
+    page.graphics.drawImage(PdfBitmap(await readImageData('IMG_5504.jpeg')), Rect.fromLTWH(0, 50, 450, 550));
+
+    PdfGrid grid = PdfGrid();
+    grid.style = PdfGridStyle(
+      cellPadding: PdfPaddings(left: 2, right: 2, top: 2, bottom: 2),
+      // cellSpacing: 2,
+      font: PdfStandardFont(PdfFontFamily.helvetica, 12),
+    );
+
+    grid.columns.add(count: 3);
+    grid.headers.add(1);
+
+    PdfGridRow header = grid.headers[0];
+    header.cells[0].value = 'ID';
+    header.cells[1].value = 'Name';
+    header.cells[2].value = 'Designation';
+
+    PdfGridRow row = grid.rows.add();
+    row.cells[0].value = 'E01';
+    row.cells[1].value = 'Clay';
+    row.cells[2].value = 'Developer';
+
+    row = grid.rows.add();
+    row.cells[0].value = 'E02';
+    row.cells[1].value = 'John';
+    row.cells[2].value = 'Tester';
+
+    // grid.draw(page: page, bounds: Rect.fromLTWH(0, 600, 450, 100));
+    grid.draw(page: document.pages.add(), bounds: Rect.fromLTWH(0, 0, 0, 0));
+
+    
+    List<int> bytes = await document.save();
+    document.dispose();
+
+    saveAndLaunchFile(bytes, 'Output.pdf');
+  }
+
+  Future<Uint8List>  readImageData(String name) async {
+    final data = await rootBundle.load('images/$name');
+    return data.buffer.asUint8List();
   }
 }
